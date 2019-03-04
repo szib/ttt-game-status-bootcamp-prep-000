c09042255c10251b0506a87b7bba6532ab8cd99a
Ivan Szebenszki <szib77@gmail.com>
2019-03-04 21:50:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,9 +18,9 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |a_combination|
     x = a_combination.map { |x| board[x] }
-    puts x.inspect
+    puts "x: #{x.inspect}"
     if x.all?("X") || x.all?("O")
-      puts "true: #{x.inspect}"
+      puts "true"
     end
   end
   false
